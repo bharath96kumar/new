5bbac8c167c3b571b73ef18d614a3378e88d7166
bharath96kumar <kumarbharath340@gmail.com>
2019-12-15 13:13:17 +0530

    done

---
+++ src/pom/GitNew.java
@@ -0,0 +1,16 @@
+package pom;
+
+public class GitNew {
+
+	public static void main(String[] args) {
+		int a=10,b=20;
+		int c=a+b;
+		if (c==30) {
+			System.out.println("correct");
+		}else {
+			System.out.println("Incorrect");
+		}
+
+	}
+
+}
